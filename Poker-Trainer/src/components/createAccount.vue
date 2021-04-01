ad88063fddc512570962a1561e6bd3eaca311bd1
johnc449 <71931044+johnc449@users.noreply.github.com>
2021-04-01 14:27:30 +0100

    Create createAccount.vue
    Form for making new account

---
+++ Poker-Trainer/src/components/createAccount.vue
@@ -0,0 +1,57 @@
+<template>
+<v-app>
+    <div>
+      <h1> Here at Upper Hand we are always looking for new members! <br> Submit your details below to join us...</h1>
+    </div> 
+    <v-text-field
+                v-model="email"
+                :error-messages="errors"
+                label="Email"
+                required
+                outlined
+                dark
+                filled
+                dense
+              ></v-text-field>
+    <v-text-field
+                v-model="username"
+                :error-messages="errors"
+                label="Username"
+                required
+                outlined
+                dark
+                filled
+                dense
+              ></v-text-field>
+     <v-text-field
+                v-model="password"
+                :error-messages="errors"
+                label="Password"
+                :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPass = !showPass"
+                required
+                outlined
+                dense
+                dark
+                filled
+                :type="showPass ? 'text' : 'password'"
+              ></v-text-field>
+   <div class="text-center">
+              <v-btn class="createAccount-btn" type="submit" rounded color="white" dark>
+                Create Account
+              </v-btn>
+            </div>
+    </v-app>  
+</template>
+
+<script>
+export default {
+    name: 'createAccount',
+    componets: {
+    },
+    data: () => ({
+    }),
+    methods: {
+    }
+}
+</script>.
